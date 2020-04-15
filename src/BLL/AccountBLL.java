48766aa5f24294624af39da8615bf8e7edd4732d
HuongVu9260 <63567470+HuongVu9260@users.noreply.github.com>
2020-04-15 09:25:39 +0700

    Update AccountBLL.java

--- src/BLL/AccountBLL.java
+++ src/BLL/AccountBLL.java
@@ -3,7 +3,7 @@ import DAL.AccountDAL;
 import java.sql.SQLException;
 public class AccountBLL {
     AccountDAL accountDAL;
-    public void Login(String UserName,String Password) throws SQLException, ClassNotFoundException{
+    public void AccountBll(String UserName,String Password) throws SQLException, ClassNotFoundException{
         accountDAL.Login(UserName, Password);
     }
 }
